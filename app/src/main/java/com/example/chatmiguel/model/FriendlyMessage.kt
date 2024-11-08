22c3e58a4f1d0f5bdce3f339a247bf91cac0db2e
MiguelEnrique94 <mienrique94@gmail.com>
2024-11-07 23:13:05 -0300

    first commit

--- app/src/main/java/com/example/chatmiguel/model/FriendlyMessage.kt
+++
@@ -1,8 +0,0 @@
-package br.com.chat_kotlin.model
-
-data class FriendlyMessage(
-    val text: String? = null,
-    val name: String? = null,
-    val photoUrl: String? = null,
-    val imageUrl: String? = null,
-)
